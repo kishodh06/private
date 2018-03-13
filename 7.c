7337ff77d0ec7c518a836f82bd1509a714877d2b
kishodh06 <30828267+kishodh06@users.noreply.github.com>
2018-03-13 16:09:02 +0530

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+     char str[50],tmp;
+     int m,n;
+     printf("Enter a string:");
+     scanf("%s",str);
+     for(m=0;m<strlen(str);m=m+2){
+     tmp = str[m];
+     str[m] = str[m+1];
+     str[m+1] = tmp;
+     }
+     printf("\n The Swapping String is : %s",str);
+     return 0;
+}
